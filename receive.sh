2dcced7c93b857732573a94c32d38030d390f1f5
timniklas <accounts@tnkoch.de>
2024-12-29 15:03:54 +0100

    Update receive.sh

--- receive.sh
+++ receive.sh
@@ -4,4 +4,4 @@
 CONFIG_PATH=/data/options.json
 FREQUENCY=$(jq --raw-output '.frequency // empty' $CONFIG_PATH)
 
-rtl_fm -d 0 -f $FREQUENCY -s 22050 -g 100 | multimon-ng -t raw -a POCSAG1200 /dev/stdin | python3 -u /app/bosmon.py
+rtl_fm -d 0 -f $FREQUENCY -s 22050 -g 100 | multimon-ng -t raw -a POCSAG1200 /dev/stdin | python3 -u /app/process.py
