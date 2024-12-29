58231dd6d21dc2a40a19b1deaf74929103cc3880
timniklas <accounts@tnkoch.de>
2024-12-29 20:18:34 +0100

    Update receive.sh

--- receive.sh
+++ receive.sh
@@ -3,5 +3,6 @@
 #get frequency from ha config
 CONFIG_PATH=/data/options.json
 FREQUENCY=$(jq --raw-output '.frequency // empty' $CONFIG_PATH)
+DEVICEID=$(jq --raw-output '.device_id // empty' $CONFIG_PATH)
 
-rtl_fm -d 0 -f $FREQUENCY -s 22050 -g 100 | multimon-ng -t raw -a POCSAG1200 /dev/stdin | python3 -u /app/process.py
+rtl_fm -d $DEVICEID -f $FREQUENCY -s 22050 -g 100 | multimon-ng -t raw -a POCSAG1200 /dev/stdin | python3 -u /app/process.py
