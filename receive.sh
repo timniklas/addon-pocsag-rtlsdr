7e6b4e3f87833ca8671219aec824e4e4cd4d6a94
timniklas <accounts@tnkoch.de>
2025-09-03 10:19:54 +0200

    lowered gain

--- receive.sh
+++ receive.sh
@@ -5,4 +5,4 @@ CONFIG_PATH=/data/options.json
 FREQUENCY=$(jq --raw-output '.frequency // empty' $CONFIG_PATH)
 DEVICEID=$(jq --raw-output '.device_id // 0' $CONFIG_PATH)
 
-rtl_fm -d $DEVICEID -f $FREQUENCY -s 22050 -g 100 | multimon-ng -t raw -a POCSAG1200 /dev/stdin | python3 -u /app/process.py
+rtl_fm -d $DEVICEID -f $FREQUENCY -s 22050 -g 32 | multimon-ng -t raw -a POCSAG1200 /dev/stdin | python3 -u /app/process.py
